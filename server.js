2b5480ebabb6675586da7a32f6c10dcd7b751fa3
k0t3n <k0t3n.mail@gmail.com>
2018-05-15 11:34:27 +0300

    Reformat graphQLServer

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const KoaMount = require('koa-mount');
 const KoaGraphQL = require('koa-graphql');
 const KoaConvert = require('koa-convert');
 
-const Schema = require('./data/schema');
+const {schema} = require('./data/schema');
 
 
 const graphQLServer = new Koa();
@@ -11,11 +11,15 @@ const graphQLServer = new Koa();
 const GRAPHQL_PORT = 3000;
 
 // TODO разобраться как работает
-graphQLServer.use(KoaMount('/', KoaConvert(KoaGraphQL({
-    schema: Schema.schema,
-    pretty: true,
-    graphiql: true,
-}))));
+graphQLServer.use(
+    KoaMount('/', KoaConvert(
+        KoaGraphQL({
+            schema: schema,
+            pretty: true,
+            graphiql: true,
+        })
+    ))
+);
 
 
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
